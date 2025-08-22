77afc40391684ee6fcedfaaa7d19c428345af223
nhanceitee-ux <nhanceitee@gmail.com>
2025-08-22 09:34:46 -0400

    Update controlFlow code

---
+++ SampleFolder/controlFlow/control_flow.js
@@ -0,0 +1,35 @@
+let userRole = "admin";
+let accessLevel;
+
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+
+console.log("Access Level:", accessLevel);
+
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+
+console.log("User Category:", userCategory);
